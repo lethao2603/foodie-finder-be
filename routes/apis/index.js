a1f232c19a72d20604f900bcb7235fe439602344
Le Thao <87373949+lethao2603@users.noreply.github.com>
2023-10-19 01:06:12 +0700

    coding register and sending verify link via email (#3)

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,5 +1,12 @@
+const router = require('express').Router();
+router.use('/test', require('./test.route'));
+//router.use('/admin/roles', require('./admin/role.route'));
+router.use('/admin/users', require('./admin/user.route'));
+router.use('/auth', require('./auth.route'));
+router.use('/customer', require('./customer/customer.route') )
+router.use('/restaurant', require("./restaurant/restaurant.router"));
+router.use('/file', require('./file.route'));
 const router = require("express").Router();
-
 // router.use('/test', require('./test.route'));
 //router.use('/admin/roles', require('./admin/role.route'));
 //router.use('/admin/users', require('./admin/user.route'));
