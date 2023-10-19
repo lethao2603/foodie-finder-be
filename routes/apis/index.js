b02cd92227dd1f306bb6258216ad70e7fe1456b8
Le Thao <lethithao666666@gmail.com>
2023-10-20 01:06:34 +0700

    resolve conflic on dev

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,13 +1,6 @@
 const router = require("express").Router();
 // router.use('/test', require('./test.route'));
-<<<<<<< Updated upstream
-router.use('/admin/users', require('./admin/user.route'));
-
-router.use('/customer', require('./customer/customer.route') )
-
-=======
 router.use("/admin/users", require("./admin/user.route"));
->>>>>>> Stashed changes
 router.use("/auth", require("./auth.route"));
 router.use("/customer", require("./customer/customer.route"));
 router.use("/restaurant", require("./restaurant/restaurant.router"));
