5aa12eafd31736eabd42e2ffef11bc8e58d1eb10
Wuynotfound <thanhhuyvo2802@gmail.com>
2023-10-20 09:56:42 +0700

    Update branch dev1

--- services/booking/bookingServices.js
+++ services/booking/bookingServices.js
@@ -3,7 +3,6 @@ const Booking = require("../../models/booking.model")
 module.exports = {
     createBooking: async (data) => {
         let result = await Booking.create(data);
-       // console.log(data.date);
         return result;
     }
 }
