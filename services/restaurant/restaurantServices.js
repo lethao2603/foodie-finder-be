541b2ea7bb3fca6101e2da5ca5f10942704cea50
Le Thao <87373949+lethao2603@users.noreply.github.com>
2023-11-16 22:10:03 +0700

    Add pagination for apis (#8)
    * fix: add pagination metadata
    
    * fix: add metadata for paganation feature

--- services/restaurant/restaurantServices.js
+++ services/restaurant/restaurantServices.js
@@ -3,21 +3,18 @@ const Category = require("../../models/category.model");
 const APIFeatures = require("../../utils/apiFeatures");
 const aqp = require("api-query-params");
 
-
 exports.createRestaurant = async (data) => {
-    let result = await Restaurant.create(data);
-    return result;
+  let result = await Restaurant.create(data);
+  return result;
 };
 
 exports.getRestaurant = async (queryString) => {
-    //EXECUTE QUERY
-    const features = new APIFeatures(Restaurant.find(), queryString)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-    const result = await features.query;
-    return result;
+  //EXECUTE QUERY
+  const features = new APIFeatures(Restaurant.find(), queryString);
+  const _ = await features.getMetadata();
+  features.filter().sort().limitFields().paginate()
+  const result = await features.query;
+  return { result, ...features.metadata, pageSize: result.length };
 };
 
 exports.getRestaurantById = async (id) => {
@@ -33,69 +30,69 @@ exports.updateRestaurant = async (id, data) => {
 };
 
 exports.deleteRestaurant = async (id) => {
-    let result = await Restaurant.deleteById(id);
-    return result;
+  let result = await Restaurant.deleteById(id);
+  return result;
 };
 
 exports.searchRestaurant = async (queryString) => {
-    const page = queryString.page;
-    const search = queryString.search;
-    const { filter, limit } = aqp(queryString);
-    let offset = (page - 1) * limit;
-    delete filter.page;
-    let result = await Restaurant.find({
-       $or: [
-        { resname: { $regex: search, $options: "i" } },
-        { typeOfRes: { $regex: search, $options: "i" } },
-        { "address.street": { $regex: search, $options: "i" } },
-        { "address.district": { $regex: search, $options: "i" } },
-        { "address.province": { $regex: search, $options: "i" } },
-      ],
-    })
-      .skip(offset)
-      .limit(limit)
-      .exec();
+  const page = queryString.page;
+  const search = queryString.search;
+  const { filter, limit } = aqp(queryString);
+  let offset = (page - 1) * limit;
+  delete filter.page;
+  let result = await Restaurant.find({
+    $or: [
+      { resname: { $regex: search, $options: "i" } },
+      { typeOfRes: { $regex: search, $options: "i" } },
+      { "address.street": { $regex: search, $options: "i" } },
+      { "address.district": { $regex: search, $options: "i" } },
+      { "address.province": { $regex: search, $options: "i" } },
+    ],
+  })
+    .skip(offset)
+    .limit(limit)
+    .exec();
 
-    return result;
+  return result;
 };
 
 exports.getResByCatgory = async (cateName) => {
-    const category = await Category.findOne({ categoryName: cateName });
-    if (category) {
-      result = await Restaurant.find({ resCateInfor: category._id });
-    } else {
-      result = [];
-    }
-    return result;
+  const category = await Category.findOne({ categoryName: cateName });
+  if (category) {
+    result = await Restaurant.find({ resCateInfor: category._id });
+  } else {
+    result = [];
+  }
+  return result;
 };
 
 exports.aliasTopRes = (req, res) => {
-    req.query.limit = '5';
-    req.query.sort = '-pointEvaluation,averagePrice';
-    req.query.fields = 'resname,address,timeOpen,timeClose,seats,typeOfRes';
+  req.query.limit = "5";
+  req.query.sort = "-pointEvaluation,averagePrice";
+  req.query.fields = "resname,address,timeOpen,timeClose,seats,typeOfRes";
 };
 
 exports.calculateRestaurantStats = async (req, res) => {
-    const stats = await Restaurant.aggregate([
-      {
-        $match: { averagePrice: { $gte: 100000 } },
+  const stats = await Restaurant.aggregate([
+    {
+      $match: { averagePrice: { $gte: 100000 } },
+    },
+    {
+      $group: {
+        _id: { $toUpper: "$typeOfRes" },
+        numTours: { $sum: 1 },
+        avgRating: { $avg: "$pointEvaluation" },
+        avgPrice: { $avg: "$averagePrice" },
+        minPrice: { $min: "$averagePrice" },
+        maxPrice: { $max: "$averagePrice" },
       },
-      {
-        $group: {
-          _id: { $toUpper: '$typeOfRes'},
-          numTours: { $sum: 1},
-          avgRating: { $avg: '$pointEvaluation' },
-          avgPrice: { $avg: '$averagePrice' },
-          minPrice: { $min: '$averagePrice' },
-          maxPrice: { $max: '$averagePrice' },  
-        },
-      },
-      {
-        $sort: {avgPrice: 1}
-      },
-      // {
-      //   $match: {_id: { $ne: 'BEEFSTEAK'}}
-      // }
-    ]);
-    return stats;
-}
+    },
+    {
+      $sort: { avgPrice: 1 },
+    },
+    // {
+    //   $match: {_id: { $ne: 'BEEFSTEAK'}}
+    // }
+  ]);
+  return stats;
+};
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,51 +1,68 @@
 class APIFeatures {
-    constructor(query, queryString) {
-        this.query = query;
-        this.queryString = queryString;
+  constructor(query, queryString) {
+    this.query = query;
+
+    this.queryString = queryString;
+    this.metadata = {
+      totalItems: 0,
+      totalPage: 0,
+      currentPage: 0,
+    };
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    //Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("_id");
     }
-  
-    filter() {
-      const queryObj = {...this.queryString};
-      const excludeFields = ['page', 'sort', 'limit', 'fields'];
-      excludeFields.forEach(el => delete queryObj[el]);
-  
-      //Advanced filtering
-      let queryStr = JSON.stringify(queryObj);
-      queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-      this.query = this.query.find(JSON.parse(queryStr));
-      
-      return this;
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select("-__v");
     }
-  
-    sort() {
-      if (this.queryString.sort) {
-        const sortBy = this.queryString.sort.split(',').join(' ');
-        this.query = this.query.sort(sortBy);
-      } else {
-        this.query = this.query.sort('-createdAt');
-      }
-      return this;
-    }
-  
-    limitFields() {
-      if(this.queryString.fields) {
-        const fields = this.queryString.fields.split(',').join(' ');
-        this.query = this.query.select(fields);
-      } else {
-        this.query = this.query.select('-__v'); 
-      }
-      return this;
-    }
-  
-    paginate() {
-      const page = this.queryString.page * 1 || 1;
-      const limit = this.queryString.limit * 1 || 100;
-      const skip = (page - 1) * limit;
-  
-      this.query = this.query.skip(skip).limit(limit);
-  
-      return this;
-    }
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+
+  async getMetadata() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const count = await this.query.countDocuments({ deleted: false });
+    this.metadata = {
+      ...this.metadata,
+      totalItems: count,
+      totalPage: Math.ceil(count / limit),
+      currentPage: page * 1 || 1,
+    };
+    console.log(this.metadata);
+    return this;
+  }
 }
 
 module.exports = APIFeatures;
