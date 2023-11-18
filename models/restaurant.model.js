1a9ac80d7c7a118bcf54eddd80bd456c329869b0
Thao Le Thi <lethithao666666@gmail.com>
2023-11-18 15:32:08 +0700

    🔥feat: build tags system based on courses

--- models/restaurant.model.js
+++ models/restaurant.model.js
@@ -1,38 +1,52 @@
 const mongoose = require("mongoose");
 const mongoose_delete = require("mongoose-delete");
-
+const Tag = require("./tag.model");
 const { DateTime } = require("mssql");
 const { AutoIncrement } = require("../config/db");
 const restaurantSchema = new mongoose.Schema(
   {
-    resname: { 
-      type: String, 
-      required: [true, 'A restaurant must have a name'], 
-      unique: true, 
-      trim: true
+    resname: {
+      type: String,
+      required: [true, "A restaurant must have a name"],
+      unique: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      trim: true,
     },
     address: {
-      street: { type: String, required: [true, 'Street must not be empty'], trim: true},
-      district: { type: String, required: [true, 'District must not be empty'], trim: true},
-      city: { type: String, required: [true, 'City must not be empty'], trim: true},
+      street: { type: String, required: [true, "Street must not be empty"], trim: true },
+      district: { type: String, required: [true, "District must not be empty"], trim: true },
+      city: { type: String, required: [true, "City must not be empty"], trim: true },
     },
-    timeOpen: { type: String, required: [true, 'timeOpen must not be empty']},
-    timeClose: { type: String, required: [true, 'timeClose must not be empty']},
-    seats: { type: Number, required: [true, 'seats must not be empty']},
+    timeOpen: { type: String, required: [true, "timeOpen must not be empty"] },
+    timeClose: { type: String, required: [true, "timeClose must not be empty"] },
+    seats: { type: Number, required: [true, "seats must not be empty"] },
     typeOfRes: {
-      type: String, 
-      required: [true, 'typeOfRes must not be empty']},
-    averagePrice: { type: Number, required: [true, 'averagePrice must not be empty']},
-    pointEvaluation: {type: Number,default: 4.5,
-      min: [1, 'Rating must be above 1.0'],
-      max: [5, 'Rating must be below 5.0'],
-      set: val => Math.round(val * 10) / 10 // 4.6666 -> 4.7
+      type: String,
+      required: [true, "typeOfRes must not be empty"],
     },
-    description: { type: String, trim: true},
-    image: {type: String, required: [true, 'image must not be empty']},
+    averagePrice: { type: Number, required: [true, "averagePrice must not be empty"] },
+    pointEvaluation: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
+      set: (val) => Math.round(val * 10) / 10, // 4.6666 -> 4.7
+    },
+    tags: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "tag",
+      },
+    ],
+    description: { type: String, trim: true },
+    image: { type: String, required: [true, "image must not be empty"] },
     resMenuInfor: { type: mongoose.Schema.Types.ObjectId, ref: "menu", default: "Undefined" },
-    resCateInfor: {type: mongoose.Schema.Types.ObjectId, ref: 'category'},
-    resOwnerInfor: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
+    resCateInfor: { type: mongoose.Schema.Types.ObjectId, ref: "category" },
+    resOwnerInfor: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
 
     // reservations: [{type: mongoose.Schema.Types.ObjectId, ref: 'reservations'}],
     numericId1: {
@@ -40,27 +54,31 @@ const restaurantSchema = new mongoose.Schema(
       unique: true,
     },
   },
-  { timestamps: true }, // createAt, updateAt
+  { timestamps: true } // createAt, updateAt
 );
-restaurantSchema.path('createdAt').select(false);
-restaurantSchema.path('updatedAt').select(false);
+restaurantSchema.path("createdAt").select(false);
+restaurantSchema.path("updatedAt").select(false);
 restaurantSchema.plugin(mongoose_delete, { overrideMethods: "all" });
-restaurantSchema.index({resname: 'text',
-  typeOfRes: 'text',"address.street": 'text',
-  "address.district": 'text',"address.province": 'text',"categoryName": 'text'
+restaurantSchema.index({
+  resname: "text",
+  typeOfRes: "text",
+  "address.street": "text",
+  "address.district": "text",
+  "address.province": "text",
+  categoryName: "text",
 });
 
 //Virtual populate
-restaurantSchema.virtual('reviews', {
-  ref: 'review',
-  foreignField: 'resInfor',
-  localField: '_id'
+restaurantSchema.virtual("reviews", {
+  ref: "review",
+  foreignField: "resInfor",
+  localField: "_id",
 });
 
-restaurantSchema.pre(/^find/, function(next){
+restaurantSchema.pre(/^find/, function (next) {
   this.populate({
-      path: 'resCateInfor',
-      select: 'categoryName'
+    path: "resCateInfor",
+    select: "categoryName",
   });
   next();
 });
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-
+const testController = require('../controllers/test.controller')
 const apisRouter = require("./apis");
 
 router.use("/api", apisRouter);
-
+router.use("/test/slugify-existing-restaurants", testController.slugifyRestaurant)
+router.use("/test/build-tag-system", testController.buildTagSystem)
 module.exports = router;
